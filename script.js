d8883a3c6b9de06b553c6f616444bf842f09e6a5
pragati411 <94692439+pragati411@users.noreply.github.com>
2022-07-14 16:50:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-navbar = document.querySelector('navbar')
+const = form = document.querySelector("form"),
+nextbtn = document.querySelector(".nextbtn"),
+allInput = document.querySelectorAll(".first");
+
+
+
+
+nextbtn.addEventListener("click", ()=> {allInput.forEach(input => {
+    if(input.value != ""){
+        form.classList.add('secActive');
+
+    }else{
+        form.classList.remove('secActive');
+        alert("input is empty")
+    }
+}
+)
+})
